8c651afb489c31ed2798a387bccf9c92406afd2b
Tyler Garlick <tjgarlick@gmail.com>
2018-01-18 20:48:14 -0700

    Hooked up the api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+const BASE_URI = `http://localhost:5000`
+
+const getPets = async () => {
+  const response = await fetch(`${BASE_URI}/pets`)
+  if (response.status !== 200)
+    console.error(response)
+  return await response.json()
+}
 
 class App extends Component {
+
+  state = {
+    pets: [],
+  }
+
+  async componentDidMount() {
+    const pets = await getPets()
+    this.setState({ pets })
+  }
+
+  renderPet({ _id, name }) {
+    return (
+      <li key={_id}>{name}</li>
+    )
+  }
+
   render() {
+
+    // getPets()
+    //   .then(pets => this.setState({ pets }))
+
+
+    const { pets } = this.state
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Pets</h1>{pets.map(pet => this.renderPet(pet))}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
